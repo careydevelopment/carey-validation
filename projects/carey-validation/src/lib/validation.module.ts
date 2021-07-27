a21331ca00d8529fa70e4b1356c2622e87912d55
careydevelopment <mrbrianmcarey@gmail.com>
2021-07-27 11:31:51 -0400

    Further refactoring

--- projects/carey-validation/src/lib/validation.module.ts
+++ projects/carey-validation/src/lib/validation.module.ts
@@ -3,19 +3,22 @@ import { SimpleValidationDirective } from './directives/simple-validation.direct
 import { VALIDATION_CONFIG_TOKEN } from './models/token';
 import { ValidationConfig } from './models/validation-config';
 import { ErrorSpreeComponent } from './components/error-spree/error-spree.component';
-import { CommonModule } from '@angular/common';  
+import { CommonModule } from '@angular/common';
+import { ServerSideErrorSpreeComponent } from './components/server-side-error-spree/server-side-error-spree.component';  
 
 @NgModule({
   declarations: [
     SimpleValidationDirective,
-    ErrorSpreeComponent
+    ErrorSpreeComponent,
+    ServerSideErrorSpreeComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
     SimpleValidationDirective,
-    ErrorSpreeComponent
+    ErrorSpreeComponent,
+    ServerSideErrorSpreeComponent
   ]
 })
 export class ValidationModule {
